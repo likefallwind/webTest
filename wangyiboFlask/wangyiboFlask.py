bd773c9f56540d0861e5511008b0e8ba2d9501fa
likefallwind <likefallwind@163.com>
2020-09-18 14:11:23 +0800

    some change

--- wangyiboFlask/wangyiboFlask.py
+++ wangyiboFlask/wangyiboFlask.py
@@ -119,4 +119,5 @@ def writePoemNew(name = "王一博"):
     return stringName
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80, debug=False)
+    #app.run(host = '0.0.0.0', port = 80, debug=False)
+    app.run(port = 8000, debug=False)
